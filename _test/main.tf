252a0233eb6efe6189f5d0d71a2f4f5a3962b0aa
Jan Sebastian Siwy <git@jansiwy.de>
2022-01-13 12:57:46 +0100

    create dedicated test

---
+++ _test/main.tf
@@ -0,0 +1,15 @@
+module "acm" {
+  source  = "./.."
+
+  primary_domain_name = "example.com"
+
+  domain_names_to_zone_ids = {
+    "example.com"     = "XYZXYZXYZXYZXYZ"
+    "www.example.com" = "YZXYZXYZXYZXYZX"
+  }
+
+  tags = {
+    app  = "some-service"
+    env  = "production"
+  }
+}
