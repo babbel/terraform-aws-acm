7004f16348846f5da942223f17f1482ad4a527bc
Jan Sebastian Siwy <git@jansiwy.de>
2022-01-13 12:46:02 +0100

    Initial version

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## v1.0.0
+
+- [Initial version](https://github.com/babbel/terraform-aws-acm/pull/1)
---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "this_acm_certificate_arn" {
+  description = "The ARN of the certificate"
+  value       = aws_acm_certificate_validation.this.certificate_arn
+}
